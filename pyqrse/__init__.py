88ac51433c85497f0af2bd4e62415ac45c105486
econokeith <keith.blackwell@gmail.com>
2019-07-21 15:04:52 -0400

    added _ to some defaults and added todos

--- pyqrse/__init__.py
+++ pyqrse/__init__.py
@@ -1,8 +1,10 @@
 from .model import *
 from .model import QRSEModel as QRSE
+
 import pyqrse.kernels as kernels
-from pyqrse.utilities.defaults import *
 import pyqrse.fittools as fittools
+
+from pyqrse.utilities.defaults import *
 
 import pyqrse.utilities.mathstats as mathstats
 import pyqrse.utilities.helpers as helpers
--- pyqrse/utilities/defaults.py
+++ pyqrse/utilities/defaults.py
@@ -1,4 +1,5 @@
 __author__='Keith Blackwell'
+
 import sys, os
 import configparser
 import copy
@@ -8,35 +9,42 @@ from collections import defaultdict
 __all__ = ['reset_label_settings', 'view_label_settings', 'LABEL_SETTINGS']
 
 # this module sets package wide defaults to
-# allow for consistent labels and plot styles across instances
+# allow for consistent labels and plot styles across instances of the
+# QRSE Model
+
+# TODO: make a default label setter
+# TODO: add more defaults i.e. linestyle, lw, etc.
+# TODO: Make a 'frozen' dict class for settings that doesn't allow new keys
 
 # find path
 # to defaults
-this = sys.modules[__name__] #this controls the path not just the value
-DATA_PATH = os.path.join(os.path.split(__file__)[0], 'DEFAULTS.ini')
+_this = sys.modules[__name__] #this controls the path not just the value
+_DATA_PATH = os.path.join(os.path.split(__file__)[0], 'DEFAULTS.ini')
 
 # use configparsert to turn it into dictionary
-parser = configparser.ConfigParser()
-parser.read(DATA_PATH)
+_parser = configparser.ConfigParser()
+_parser.read(_DATA_PATH)
 
 _LABEL_DEFAULTS = defaultdict(dict)
 
-for key0, value0 in parser.items():
-    if value0:
-        for key1, value1 in value0.items():
-            _LABEL_DEFAULTS [key0][key1] = ast.literal_eval(value1)
+for _key0, _value0 in _parser.items():
+    if _value0:
+        for _key1, _value1 in _value0.items():
+            _LABEL_DEFAULTS [_key0][_key1] = ast.literal_eval(_value1)
 
 # this is the actual package wide value used
-this.LABEL_SETTINGS = copy.deepcopy(_LABEL_DEFAULTS)
+_this.LABEL_SETTINGS = copy.deepcopy(_LABEL_DEFAULTS)
+
+
 
 def reset_label_settings():
     """
     reset labels/color/etc settings to default loaded from DEFAULTS.ini
     """
-    this.LABEL_SETTINGS = copy.deepcopy(_LABEL_DEFAULTS)
+    _this.LABEL_SETTINGS = copy.deepcopy(_LABEL_DEFAULTS)
 
 def view_label_settings():
-    for key0, value0 in this.LABEL_SETTINGS.items():
+    for key0, value0 in _this.LABEL_SETTINGS.items():
         if value0:
             print(key0)
             for key1, value1 in value0.items():
